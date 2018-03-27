6a95138fa5a2d8ecc2ff6f448de91d601298e165
ghareeb-falazi <ghareeb.falazi@hotmail.com>
2018-03-27 18:04:10 +0200

    initialize executable camunda project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# BlockME-UseCase
+# BlockME Use-Case
+The converted BPMN 2.0 model can be found in 
+```
+src/main/resources/crypto-exchange.bpmn
+```
 A visualization of the BPMN model can be created using [bpmn.io](https://demo.bpmn.io/).
---
+++ src/main/java/blockchains/iaas/uni/stuttgart/de/CryptoExchangeApplication.java
@@ -0,0 +1,20 @@
+package blockchains.iaas.uni.stuttgart.de;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+/********************************************************************************
+ * Copyright (c) 2018 Institute for the Architecture of Application System -
+ * University of Stuttgart
+ * Author: Ghareeb Falazi
+ *
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+@ProcessApplication("Crypto Exchange App")
+public class CryptoExchangeApplication extends ServletProcessApplication {
+    // empty implementation
+}
